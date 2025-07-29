1ad3615daea30f348426bf771196c57c2de7f9d3
haldaniko <haldaniko@gmail.com>
2025-07-29 17:34:24 +0300

    minor fix for test

--- website/views.py
+++ website/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 
 def video_view(request):
-    return render(request, 'video.html', {'dash_manifest_url': '/media/manifest.mpd'})
+    return render(request,
+                  'video.html',
+                  {'dash_manifest_url': '/media/video1/manifest.mpd'})
