47b3f99844c82d8ad6d633b6c186a763e112cbd1
haldaniko <haldaniko@gmail.com>
2025-08-04 17:03:03 +0300

    add different pages for different languages

--- website/views.py
+++ website/views.py
@@ -2,6 +2,18 @@ from django.shortcuts import render
 
 
 def video_view(request):
-    return render(request,
-                  'video.html',
-                  {'dash_manifest_url': '/media/video1/manifest.mpd'})
+    lang = request.GET.get('lang', 'en')
+
+    template_map = {
+        'en': 'video_en.html',
+        'fr': 'video_fr.html',
+        'tr': 'video_tr.html',
+        'ua': 'video_ua.html',
+        'bg': 'video_bg.html',
+    }
+
+    template_name = template_map.get(lang, 'video_en.html')
+
+    return render(request, template_name, {
+        'dash_manifest_url': '/media/video1/manifest.mpd'
+    })
