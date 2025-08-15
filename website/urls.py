ae9d5478f31e8cbad84ac80fd728d8a343e446f4
haldaniko <haldaniko@gmail.com>
2025-08-15 17:01:42 +0300

    adapt header for mobile

--- website/urls.py
+++ website/urls.py
@@ -1,10 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from .views import video_view
+from .views import video_view, test_view
 
 urlpatterns = [
     path('', video_view, name='video'),
+    path("mobile/", test_view, name="mobile")
 ]
 
 if settings.DEBUG:
